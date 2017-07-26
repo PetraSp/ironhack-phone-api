f05c8998bfe39e4f41d1d619a1a612f463c1b1f0
Tair Assimov <tair@assimov.net>
2017-07-27 01:13:06 +0200

    Add db config

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+// database connection
+require('./config/database');
+
 var app = express();
 
 // view engine setup
