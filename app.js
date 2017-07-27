85e9626208cdd762a8a51107087c07522cdaa2c8
Tair Assimov <tair@assimov.net>
2017-07-28 00:13:51 +0200

    Clean up

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var passport = require('./config/passport');
 
-var index = require('./routes/index');
 var phonesApi = require('./routes/phones-api');
 var userAuth = require('./routes/user-auth');
 
@@ -19,23 +16,12 @@ var app = express();
 
 app.use(cors());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', userAuth);
 app.use('/api',  passport.authenticate('jwt', {session: false}), phonesApi);
-app.use(function(req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -50,9 +36,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({ message: err.message });
 });
 
 module.exports = app;
