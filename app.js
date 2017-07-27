1ca71b529a21faf5b46141bd3822fb5d7423e03a
Tair Assimov <tair@assimov.net>
2017-07-27 02:31:30 +0200

    Handle non existing route

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', phonesApi);
+app.use(function(req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
