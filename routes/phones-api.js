b7ac12a418d02a1ea4598f6056973710c6dd050c
Tair Assimov <tair@assimov.net>
2017-07-27 02:31:05 +0200

    Add 3 more endpoints

--- routes/phones-api.js
+++ routes/phones-api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Phone = require('../models/phone-model');
 
@@ -32,6 +33,65 @@ router.post('/phones', (req, res, next) => {
       id: thePhone._id
     });
   });
+});
+
+router.get('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Phone.findById(req.params.id, (err, thePhone) => {
+      if (err) {
+        res.json(err);
+        return;
+      }
+
+      res.json(thePhone);
+    });
+});
+
+router.put('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  const updates = {
+    brand: req.body.brand,
+    name: req.body.name,
+    specs: req.body.specs,
+    image: req.body.image
+  };
+
+  Phone.findByIdAndUpdate(req.params.id, updates, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: 'Phone updated successfully'
+    });
+  });
+})
+
+router.delete('/phones/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Phone.remove({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    return res.json({
+      message: 'Phone has been removed!'
+    });
+  })
 });
 
 module.exports = router;
